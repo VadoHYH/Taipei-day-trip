cdfc7f381a404bc82a6217de57a431e6dd636f7e
Vado-hyh <vul4ul4cjo@gmail.com>
2025-03-14 18:39:52 +0800

    fix mysql connection

--- taipei-day-trip/database.py
+++ taipei-day-trip/database.py
@@ -3,9 +3,9 @@ import pymysql
 # 建立 MySQL 連線
 def get_db_connection():
     return pymysql.connect(
-        host="localhost",
+        host="ip-172-31-42-179",
         user="root",
-        password="vadomysql",
+        password="ec2wehelpmysql",
         database="taipei_trip",
         cursorclass=pymysql.cursors.DictCursor
     )
