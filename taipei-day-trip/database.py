ab641d5780501fbe7350e266f9801c73e24ce7a7
Vado-hyh <vul4ul4cjo@gmail.com>
2025-03-21 19:54:49 +0800

    change sql password

--- taipei-day-trip/database.py
+++ taipei-day-trip/database.py
@@ -5,7 +5,7 @@ def get_db_connection():
     return pymysql.connect(
         host="localhost",
         user="root",
-        password="vadomysql",
+        password="ec2wehelpmysql",
         database="taipei_trip",
         cursorclass=pymysql.cursors.DictCursor
     )
