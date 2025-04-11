61b43d17457f433ba6647053619577ce54be4fe2
Vado-hyh <vul4ul4cjo@gmail.com>
2025-04-11 16:18:45 +0800

    edited css

--- taipei-day-trip/app.py
+++ taipei-day-trip/app.py
@@ -10,7 +10,7 @@ app.include_router(router)
 # **提供靜態檔案（CSS、JS、圖片）**
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="static")
+templates = Jinja2Templates(directory="templates")
 
 # Static Pages (Never Modify Code in this Block)
 @app.get("/", include_in_schema=False)
@@ -22,6 +22,5 @@ async def attraction(request: Request):
 @app.get("/booking", include_in_schema=False)
 async def booking(request: Request):
 	return FileResponse("./static/booking.html", media_type="text/html")
-@app.get("/thankyou", include_in_schema=False)
 async def thankyou(request: Request):
 	return FileResponse("./static/thankyou.html", media_type="text/html")
