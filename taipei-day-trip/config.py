14fbeb3d11ac1706597d9b97b35ee6087325954d
Vado-hyh <vul4ul4cjo@gmail.com>
2025-04-24 17:16:45 +0800

    fix FB

--- taipei-day-trip/config.py
+++ taipei-day-trip/config.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 # 明確指定 .env 的位置（與啟動檔同目錄）
 env_path = Path(__file__).resolve().parent / ".env"
-load_dotenv(dotenv_path=env_path)
 
 load_dotenv()  # 載入環境變數
 
@@ -20,3 +19,5 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "taipei_trip")
 
+print("DB_USER =", DB_USER)
+print("DB_PASSWORD =", DB_PASSWORD)
