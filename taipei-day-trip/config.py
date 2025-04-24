bae345d717a3bac68baed9b01e8e52a7c411e830
Vado-hyh <vul4ul4cjo@gmail.com>
2025-04-24 16:28:21 +0800

    change db_password

--- taipei-day-trip/config.py
+++ taipei-day-trip/config.py
@@ -7,13 +7,10 @@ SECRET_KEY = os.getenv("SECRET_KEY", "default-fallback-key")  # 若環境變數�
 ALGORITHM = "HS256"
 TOKEN_EXPIRE_DAYS = 7
 
-MERCHANT_KEY=os.getenv("MERCHANT_KEY", "Vadohyh_CTBC")
-
-PARTNER_KEY = "partner_IUVM060Pn8TyybxpvyDItaPiv4vv5622ruZHPIOJThxmS3AtnS35igfC"
+MERCHANT_KEY=os.getenv("MERCHANT_KEY", "")
+PARTNER_KEY = os.getenv("PARTNER_KEY", "") 
 
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "vadomysql")#vadomysql#ec2wehelpmysql
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")#vadomysql#ec2wehelpmysql
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "taipei_trip")
-
-ATTRACTION_API_URL= os.getenv("apiUrl", "http://127.0.0.1:8000/api/attractions/${attractionId}")
